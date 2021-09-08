c45d0f5d198d8c03be54ad3471d76c05bd38c9b4
Eugene Skrobov <skrobov.ev@gmail.com>
2021-09-08 13:29:20 +0700

    Создает статические view для хедера

--- src/presenters/application.js
+++ src/presenters/application.js
@@ -3,11 +3,10 @@ import { render } from '../utils/render.js';
 import HeaderView from '../views/header.js';
 import HeaderContainerView from '../views/header-container.js';
 import TripMainView from '../views/trip-main.js';
+import TripInfoView from '../views/trip-info.js';
 import TripControlsView from '../views/trip-controls.js';
-import NavigationContainerView from '../views/navigation-container.js';
-// Navigation
-import FiltersContainerView from '../views/filters-container.js';
-// Filters
+import NavigationView from '../views/navigation.js';
+import FiltersView from '../views/filters.js';
 import EventAddButtonView from '../views/event-add-button.js';
 import MainView from '../views/main.js';
 import ContainerView from '../views/container.js';
@@ -22,9 +21,10 @@ export default class ApplicationPresenter {
     this._headerView = new HeaderView();
     this._headerContainerView = new HeaderContainerView();
     this._tripMainView = new TripMainView();
+    this._tripInfoView = new TripInfoView();
     this._tripControlsView = new TripControlsView();
-    this._navigationContainerView = new NavigationContainerView();
-    this._filtersContainerView = new FiltersContainerView();
+    this._navigationView = new NavigationView();
+    this._filtersView = new FiltersView();
     this._eventAddButtonView = new EventAddButtonView();
     this._mainView = new MainView();
     this._containerView = new ContainerView();
@@ -44,14 +44,15 @@ export default class ApplicationPresenter {
 
   _renderTripMain() {
     render(this._headerContainerView, this._tripMainView);
+    render(this._tripMainView, this._tripInfoView);
     this._renderTripControls();
     render(this._tripMainView, this._eventAddButtonView);
   }
 
   _renderTripControls() {
     render(this._tripMainView, this._tripControlsView);
-    render(this._tripControlsView, this._navigationContainerView);
-    render(this._tripControlsView, this._filtersContainerView);
+    render(this._tripControlsView, this._navigationView);
+    render(this._tripControlsView, this._filtersView);
   }
 
   _renderMain() {
---
+++ src/views/navigation.js
@@ -0,0 +1,17 @@
+import AbstractView from './abstract.js';
+
+export const createNavigationTemplate = () => `
+  <div class="trip-controls__navigation">
+    <h2 class="visually-hidden">Switch trip view</h2>
+    <nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>
+  </div>
+`;
+
+export default class NavigationView extends AbstractView {
+  getTemplate() {
+    return createNavigationTemplate();
+  }
+}
