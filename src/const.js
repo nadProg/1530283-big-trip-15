f971f03b0bb694ddc8cb51abb5fd341df0a8ea04
Eugene Skrobov <skrobov.ev@gmail.com>
2021-09-14 12:36:54 +0700

    Рефакторит структуру const

--- src/const.js
+++ src/const.js
@@ -8,6 +8,101 @@ export const Place = {
   AFTER_BEGIN: 'afterbegin',
 };
 
+export const APIMethod = {
+  GET: 'GET',
+  PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
+};
+
+export const SuccessHTTPStatusRange = {
+  MIN: 200,
+  MAX: 299,
+};
+
+export const Message = {
+  LOADING: 'Loading...',
+  CREATE_POINT: 'Click New Event to create your first point',
+  NO_FUTURE_POINTS: 'There are no future events now',
+  NO_PAST_POINTS: 'There are no past events now',
+  CREATE_IN_OFLLINE: 'You can\'t create new points in offline mode',
+  EDIT_IN_OFFLINE: 'You can\'t edit points in offline mode',
+  DELETE_IN_OFFLINE: 'You can\'t delete points in offline mode',
+  ONLINE: 'On-line mode',
+  OFFLINE: 'Off-line mode',
+  NO_DATA_TO_CREATE: 'No available data to create new points',
+  NO_DATA_TO_EDIT: 'No available data to edit points',
+};
+
+export const Screen = {
+  TABLE: 'table',
+  STATISCTICS: 'statisctics',
+};
+
+export const FilterType = {
+  ALL: 'everything',
+  FUTURE: 'future',
+  PAST: 'past',
+};
+
+export const SortType = {
+  DAY: 'day',
+  EVENT: 'event',
+  TIME: 'time',
+  PRICE: 'price',
+  OFFERS: 'offers',
+};
+
+export const PointType = {
+  TAXI: 'Taxi',
+  BUS: 'Bus',
+  TRAIN: 'Train',
+  SHIP: 'Ship',
+  DRIVE: 'Drive',
+  FLIGHT: 'Flight',
+  CHECK_IN: 'Check-in',
+  SIGHTSEEING: 'Sightseeing',
+  RESTAURANT: 'Restaurant',
+};
+
+export const StatiscticsType = {
+  MONEY: 'money',
+  TYPE: 'type',
+  TIME_SPEND: 'time-spend',
+};
+
+export const UserAction = {
+  CREATE_POINT: 'create-point',
+  UPDATE_POINT: 'update-point',
+  DELETE_POINT: 'delete-point',
+};
+
+export const UpdateType = {
+  INIT: 'init',
+  PATCH: 'patch',
+  MINOR: 'minor',
+  MAJOR: 'major',
+};
+
+// Change default start and end date
+export const DEFAULT_POINT = {
+  date: {
+    end: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    start: new Date(),
+  },
+  basePrice: 100,
+  destination: null,
+  destinationName: '',
+  offers: [],
+  isNew: true,
+  isFavorite: false,
+};
+
+export const COMMON_DATEPICKER_OPTIONS = {
+  enableTime: true,
+  dateFormat: 'd/m/y H:i',
+};
+
 export const STATISCTICS_CHART_TYPE = 'horizontalBar';
 
 export const STATISTICS_CHART_BAR_HEIGHT = 55;
@@ -65,104 +160,10 @@ export const END_POINT = 'https://15.ecmascript.pages.academy/big-trip';
 
 export const AUTHORIZATION = 'Basic 5Rj6gkb1dsf53b53b';
 
-export const APIMethod = {
-  GET: 'GET',
-  PUT: 'PUT',
-  POST: 'POST',
-  DELETE: 'DELETE',
-};
-
-export const SuccessHTTPStatusRange = {
-  MIN: 200,
-  MAX: 299,
-};
-
-export const PointType = {
-  TAXI: 'Taxi',
-  BUS: 'Bus',
-  TRAIN: 'Train',
-  SHIP: 'Ship',
-  DRIVE: 'Drive',
-  FLIGHT: 'Flight',
-  CHECK_IN: 'Check-in',
-  SIGHTSEEING: 'Sightseeing',
-  RESTAURANT: 'Restaurant',
-};
-
-export const SortType = {
-  DAY: 'day',
-  EVENT: 'event',
-  TIME: 'time',
-  PRICE: 'price',
-  OFFERS: 'offers',
-};
-
-export const FilterType = {
-  ALL: 'everything',
-  FUTURE: 'future',
-  PAST: 'past',
-};
-
-export const Screen = {
-  TABLE: 'table',
-  STATISCTICS: 'statisctics',
-};
-
-export const DEFAULT_POINT = {
-  date: {
-    end: new Date(Date.now() + 1000 * 60 * 60 * 24),
-    start: new Date(),
-  },
-  basePrice: 100,
-  destination: null,
-  destinationName: '',
-  offers: [],
-  isNew: true,
-  isFavorite: false,
-};
-
-export const UserAction = {
-  CREATE_POINT: 'create-point',
-  UPDATE_POINT: 'update-point',
-  DELETE_POINT: 'delete-point',
-};
-
-export const UpdateType = {
-  INIT: 'init',
-  PATCH: 'patch',
-  MINOR: 'minor',
-  MAJOR: 'major',
-};
-
-export const StatiscticsType = {
-  MONEY: 'money',
-  TYPE: 'type',
-  TIME_SPEND: 'time-spend',
-};
-
-export const COMMON_DATEPICKER_OPTIONS = {
-  enableTime: true,
-  dateFormat: 'd/m/y H:i',
-};
-
-export const Message = {
-  LOADING: 'Loading...',
-  CREATE_POINT: 'Click New Event to create your first point',
-  NO_FUTURE_POINTS: 'There are no future events now',
-  NO_PAST_POINTS: 'There are no past events now',
-  CREATE_IN_OFLLINE: 'You can\'t create new points in offline mode',
-  EDIT_IN_OFFLINE: 'You can\'t edit points in offline mode',
-  DELETE_IN_OFFLINE: 'You can\'t delete points in offline mode',
-  ONLINE: 'On-line mode',
-  OFFLINE: 'Off-line mode',
-  NO_DATA_TO_CREATE: 'No available data to create new points',
-  NO_DATA_TO_EDIT: 'No available data to edit points',
-};
+export const OFFLINE_POSTFIX = ' [offline]';
 
 export const filterTypeToMessage = {
   past: Message.NO_PAST_POINTS,
   future: Message.NO_FUTURE_POINTS,
   everything: Message.CREATE_POINT,
 };
-
-export const OFFLINE_POSTFIX = ' [offline]';
